c104a828efe62cfa1e83400314ada6981e255ab7
A. Bolitho <ab2075@pcmathsg08.maths.pwf.cam.ac.uk>
2017-10-30 16:14:01 +0000

    added interface for biased goodie

--- goodies.py
+++ goodies.py
@@ -22,3 +22,21 @@ class RandomGoody(Goody):
         ''' Ignore any ping information, just choose a random direction to walk in, or ping '''
         possibilities = [direction for direction in [UP, DOWN, LEFT, RIGHT] if not obstruction[direction]] + [PING]
         return random.choice(possibilities)
+
+class BiasedGoody(Goody):
+    ''' A goody that is biased to walk towards one another '''
+
+    def take_turn(self, obstruction, _ping_response):
+        ''' ping every 10 turns and walk towards each other '''
+        pass
+
+    def player_bias(self, _ping_response):
+	''' decides which direction to move towards based on where the other player is'''
+	pass
+
+    def get_player_relative_position(self, _ping_response):
+	''' extracts where the other player is from the _ping_response input using isinstance to
+	distinguish between goodie and baddie'''
+	pass
+	
+
